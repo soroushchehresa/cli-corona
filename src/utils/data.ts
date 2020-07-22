f2c4914d920d9e856e4fc03a91841323272ddcf7
soroushchehresa <s1996ch@gmail.com>
2020-07-22 23:43:18 +0430

    v1.4.0

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,12 +1,14 @@
-import * as covid from 'novelcovid';
+import api from 'novelcovid';
+
+api.settings({ baseUrl: 'https://disease.sh' });
 
 export default class Data {
   static async get(country: string): Promise<{data: object}> {
     let covidData: object = {};
     if (country === 'all') {
-      covidData = await covid.getAll();
+      covidData = await api.all();
     } else {
-      covidData = await covid.getCountry({ country });
+      covidData = await api.countries({ country, allowNull: false, sort: 'cases', strict: false });
     }
     return { data: covidData };
   }
